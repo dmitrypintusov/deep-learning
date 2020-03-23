9d14f1000b921f4b75c28abc4018fb0bc3d9693a
Dzmitry Pintusau <dzmitrypintusov@gmail.com>
2020-03-23 22:40:15 +0300

    Added lab5

---
+++ by/bsuir/pintusov/labs/lab5/lab5.py
@@ -0,0 +1,237 @@
+import os
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+from keras.preprocessing.image import load_img
+from sklearn.model_selection import train_test_split
+
+print(os.listdir("./input"))
+
+filenames = os.listdir("./input/train")
+categories = []
+for filename in filenames:
+    category = filename.split('.')[0]
+    if category == 'dog':
+        categories.append("1")
+    else:
+        categories.append("0")
+
+df = pd.DataFrame({
+    'filename': filenames,
+    'category': categories
+})
+df.head()
+
+df['category'].value_counts().plot.bar()
+plt.show()
+
+sample = random.choice(filenames)
+image = load_img("./input/train/" + sample)
+plt.imshow(image)
+plt.show()
+
+from keras import layers
+from keras.layers import Dropout, Dense, GlobalMaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.applications import VGG16
+from keras.models import Model
+
+image_size = 224
+input_shape = (image_size, image_size, 3)
+
+epochs = 5
+batch_size = 16
+
+pre_trained_model = VGG16(input_shape=input_shape, include_top=False, weights="imagenet")
+
+for layer in pre_trained_model.layers[:15]:
+    layer.trainable = False
+
+for layer in pre_trained_model.layers[15:]:
+    layer.trainable = True
+
+last_layer = pre_trained_model.get_layer('block5_pool')
+last_output = last_layer.output
+
+# Flatten the output layer to 1 dimension
+x = GlobalMaxPooling2D()(last_output)
+# Add a fully connected layer with 512 hidden units and ReLU activation
+x = Dense(512, activation='relu')(x)
+# Add a dropout rate of 0.5
+x = Dropout(0.5)(x)
+# Add a final sigmoid layer for classification
+x = layers.Dense(1, activation='sigmoid')(x)
+
+model = Model(pre_trained_model.input, x)
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+              metrics=['accuracy'])
+
+model.summary()
+
+train_df, validate_df = train_test_split(df, test_size=0.1)
+train_df = train_df.reset_index()
+validate_df = validate_df.reset_index()
+
+total_train = train_df.shape[0]
+total_validate = validate_df.shape[0]
+
+train_datagen = ImageDataGenerator(
+    rotation_range=15,
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest',
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
+
+train_generator = train_datagen.flow_from_dataframe(
+    train_df,
+    "./input/train/",
+    x_col='filename',
+    y_col='category',
+    class_mode='binary',
+    target_size=(image_size, image_size),
+    batch_size=batch_size
+)
+
+validation_datagen = ImageDataGenerator(rescale=1. / 255)
+validation_generator = validation_datagen.flow_from_dataframe(
+    validate_df,
+    "./input/train",
+    x_col='filename',
+    y_col='category',
+    class_mode='binary',
+    target_size=(image_size, image_size),
+    batch_size=batch_size
+)
+
+example_df = train_df.sample(n=1).reset_index()
+example_generator = train_datagen.flow_from_dataframe(
+    example_df,
+    "./input/train/",
+    x_col='filename',
+    y_col='category'
+)
+plt.figure(figsize=(12, 12))
+for i in range(0, 9):
+    plt.subplot(3, 3, i + 1)
+    for X_batch, Y_batch in example_generator:
+        image = X_batch[0]
+        plt.imshow(image)
+        break
+plt.tight_layout()
+plt.show()
+
+# fine-tune the model
+history = model.fit_generator(
+    train_generator,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=total_validate // batch_size,
+    steps_per_epoch=total_train // batch_size)
+
+loss, accuracy = model.evaluate_generator(validation_generator, total_validate // batch_size, workers=12)
+print("Test: accuracy = %f  ;  loss = %f " % (accuracy, loss))
+
+
+def plot_model_history(model_history, acc='acc', val_acc='val_acc'):
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+    axs[0].plot(range(1, len(model_history.history[acc]) + 1), model_history.history[acc])
+    axs[0].plot(range(1, len(model_history.history[val_acc]) + 1), model_history.history[val_acc])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1, len(model_history.history[acc]) + 1), len(model_history.history[acc]) / 10)
+    axs[0].legend(['train', 'val'], loc='best')
+    axs[1].plot(range(1, len(model_history.history['loss']) + 1), model_history.history['loss'])
+    axs[1].plot(range(1, len(model_history.history['val_loss']) + 1), model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1, len(model_history.history['loss']) + 1), len(model_history.history['loss']) / 10)
+    axs[1].legend(['train', 'val'], loc='best')
+    plt.show()
+
+
+plot_model_history(history)
+
+Y_val = validate_df['category']
+y_pred = model.predict_generator(validation_generator)
+threshold = 0.5
+y_final = np.where(y_pred > threshold, 1, 0)
+y_final.size
+
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+# Predict the values from the validation dataset
+
+# compute the confusion matrix
+confusion_mtx = confusion_matrix(Y_val, y_final)
+# plot the confusion matrix
+f, ax = plt.subplots(figsize=(8, 8))
+sns.heatmap(confusion_mtx, annot=True, linewidths=0.01, cmap="Greens", linecolor="gray", fmt='.1f', ax=ax)
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+plt.title("Confusion Matrix")
+plt.show()
+
+from sklearn.metrics import classification_report
+
+# Generate a classification report
+report = classification_report(Y_val, y_final, target_names=['0', '1'])
+
+print(report)
+
+test_filenames = os.listdir("./input/test1/")
+test_df = pd.DataFrame({
+    'filename': test_filenames
+})
+nb_samples = test_df.shape[0]
+
+test_gen = ImageDataGenerator(rescale=1. / 255)
+test_generator = test_gen.flow_from_dataframe(
+    test_df,
+    "./input/test1/",
+    x_col='filename',
+    y_col=None,
+    class_mode=None,
+    batch_size=batch_size,
+    target_size=(image_size, image_size),
+    shuffle=False
+)
+
+predict = model.predict_generator(test_generator, steps=np.ceil(nb_samples / batch_size))
+threshold = 0.5
+test_df['category'] = np.where(predict > threshold, 1, 0)
+
+sample_test = test_df.sample(n=9).reset_index()
+sample_test.head()
+plt.figure(figsize=(12, 12))
+for index, row in sample_test.iterrows():
+    filename = row['filename']
+    category = row['category']
+    img = load_img("../input/test1/test1/" + filename, target_size=(256, 256))
+    plt.subplot(3, 3, index + 1)
+    plt.imshow(img)
+    plt.xlabel(filename + '(' + "{}".format(category) + ')')
+plt.tight_layout()
+plt.show()
+
+submission_df = test_df.copy()
+submission_df['id'] = submission_df['filename'].str.split('.').str[0]
+submission_df['label'] = submission_df['category']
+submission_df.drop(['filename', 'category'], axis=1, inplace=True)
+submission_df.to_csv('submission_13010030.csv', index=False)
+
+plt.figure(figsize=(10, 5))
+sns.countplot(submission_df['label'])
+plt.title("(Test data)")
+plt.show()
